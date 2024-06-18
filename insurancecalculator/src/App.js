ab008a27a074e84caefa6049c348555be8d86334
mikeezvz <mike.zogheib@lernende.bbw.ch>
2024-06-19 00:06:38 +0200

    homepage updated
    - index.js bearbeitet:
     - Navbar bearbeitet
     - sinnvolle Reiter zu Navbar hinzugefügt
     - Logo erstellt und eingefügt
     - Text bearbeitet
    
    - index.js edited:
     - Navbar edited
     - added meaningful tabs to Navbar
     - Logo created and inserted
     - edited text

--- insurancecalculator/src/App.js
+++ insurancecalculator/src/App.js
@@ -1,38 +1,41 @@
 import React from 'react';
+import { Navbar, Nav, Image, Container, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'; // Importiere die benutzerdefinierte CSS-Datei
+import './App.css'; // oder './index.css', je nachdem, wo deine CSS-Datei ist
+import logo from './Zogly.png';
 
 function App() {
   return (
-      <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-dark navbar-custom">
-          <a className="navbar-brand" href="#"></a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Sricing Pex</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <div className="container mt-5">
-          <h1>Welcome to React with Bootstrap</h1>
-          <p>This is a simple example of using Bootstrap in a React project.</p>
-          <button type="button" className="btn btn-primary">Primary Button</button>
-        </div>
-      </div>
+    <div className="App">
+      <Navbar expand="lg" className="navbar-custom" variant="dark">
+        <Navbar.Brand href="#main">
+          <Image src={logo} alt="Logo" fluid className="nav-logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#main">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#calculator">Calculator</Nav.Link>
+            <Nav.Link href="#about-us">About Us</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+          <Nav className="ms-auto">
+            <Nav.Link href="#main">Zogly</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Container className="mt-5">
+        <h1>Welcome to the Private Liability Insurance  Calculator</h1>
+        <br></br>
+        <br></br>
+        <p>Calculate your liability insurance coverage and costs</p>
+        <br></br>
+        <br></br>
+        <Button variant="primary" size='lg'>Get Started</Button>
+      </Container>
+    </div>
   );
 }
 
