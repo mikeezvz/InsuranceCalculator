b8a350551f2ed2457cbd170a37fd1aebe3566e3c
mikeezvz <mike.zogheib@lernende.bbw.ch>
2024-06-20 18:54:13 +0200

    Homepage finished
     - App.js sollte vollständig sein,
     Änderungen könnten noch
     unternommen werden
    - Änderungen:
     - Bilder hinzugefügt
     - Navbar im Footer hinzugefüt
     - Standorte mit Karte hinzugefügt
     - Carousel mit 3 Bildern hinzugefügt , angemessene Beschriftung erstellt
    
     - App.js should be complete,
     changes could still
     be made
    - Changes:
     - Added images
     - Navbar added in footer
     - Added locations with map
     - Carousel with 3 images added, appropriate labeling created

---
+++ insurancecalculator/src/demographics.js
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import { Navbar, Nav, Image, Container, Button, Row, Col, Dropdown, Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './calculator.css';
